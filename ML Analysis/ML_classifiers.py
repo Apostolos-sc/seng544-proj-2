3ea510468fd19f3f48591c98b80fdaab850d129d
ttdow <travis.t.dow@gmail.com>
2022-12-01 18:38:22 -0700

    Reorganized dir. Added example for LSTM implementation. Modified ML_classifiers.py and bert_twitter.py for new, larger dataset. ML_classifiers.py working okay, although SVC takes way too long. bert_twitter.py not working as new dataset is multiclass classification, Apostolos working on fix.

--- ML Analysis/ML_classifiers.py
+++ ML Analysis/ML_classifiers.py
@@ -4,18 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
-import tensorflow as tf
-from keras.utils import to_categorical
+#import tensorflow as tf
+#from keras.utils import to_categorical
 import pandas as pd
 import nltk
 
-# Import data and convert categorical data to int
+# ---------- Import data and convert categorical data to int ----------
 df = pd.read_csv("data.csv")
 countries = df['country']
 cat = pd.Categorical(df['country'])
 country_code = pd.Series(cat.codes)
 
-# One Hot Encoding of countries
+# ---------- One Hot Encoding of countries NOT IMPLEMENTED YET ----------
 #Y = to_categorical(country_code, num_classes=78)
 Y = country_code
 
@@ -37,13 +37,11 @@ def detokenize(text):
 corpus = corpus.apply(detokenize)
 print(corpus.head(5))
 
-# Bag-of-words
+# ---------- Bag-of-words ----------
 print("Vectorizing...")
-vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', min_df=(100.0/corpus.count()))
+vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', min_df=(1.0/corpus.count()))
 X = vectorizer.fit_transform(corpus)
-print(X.shape)
 
-# Divvy up the Tweet data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 print("Fitting MultinomialNB...")
@@ -51,49 +49,38 @@ gnb = MultinomialNB()
 gnb.fit(X_train, Y_train)
 print("MultinomialNB Bag-of-words score: " + str(gnb.score(X_test, Y_test)))
 
-print("Fitting SVC")
-svc = SVC(kernel='linear', gamma='auto')
-svc.fit(X_train, Y_train)
-print("SVC Bag-of-words score: " + str(svc.score(X_test, Y_test)))
-
 print("Fitting Logistic Regression...")
 lr = LogisticRegression(multi_class='multinomial')
 lr.fit(X_train, Y_train)
 print("LR Bag-of-words score: " + str(lr.score(X_test, Y_test)))
 
-"""
-vectorizer = CountVectorizer(stop_words='english')
-X = vectorizer.fit_transform(corpus) # Bag-of-words vectorization
-#print(vectorizer.get_feature_names_out())
-#print(X.toarray())
+'''
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.1, random_state=0)
+X_train2, X_test, Y_train2, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-gnb = MultinomialNB()
-gnb.fit(X_train.toarray(), Y_train)
-print("MultinomialNB Bag-of-words score: " + str(gnb.score(X_test.toarray(), Y_test)))
-
-svc = SVC()
+print("Fitting SVC")
+svc = SVC(kernel='linear', gamma='auto')
 svc.fit(X_train, Y_train)
 print("SVC Bag-of-words score: " + str(svc.score(X_test, Y_test)))
+'''
 
-lr = LogisticRegression(multi_class='multinomial')
-lr.fit(X_train, Y_train)
-print("LR Bag-of-words score: " + str(lr.score(X_test, Y_test)))
+# ---------- TF-IDF ----------
 
-vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer(strip_accents='ascii', stop_words='english', min_df=(1.0/corpus.count()))
 X = vectorizer.fit_transform(corpus)
-#print(X.toarray())
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+print("Fitting MultinomialNB...")
 gnb = MultinomialNB()
-gnb.fit(X_train.toarray(), Y_train)
-print("MultinomialNB TF-IDF score: " + str(gnb.score(X_test.toarray(), Y_test)))
+gnb.fit(X_train, Y_train)
+print("MultinomialNB TF-IDF score: " + str(gnb.score(X_test, Y_test)))
 
-svc = SVC()
-svc.fit(X_train, Y_train)
-print("SVC TF-IDF score: " + str(svc.score(X_test, Y_test)))
+#svc = SVC()
+#svc.fit(X_train, Y_train)
+#print("SVC TF-IDF score: " + str(svc.score(X_test, Y_test)))
 
+print("Fitting Logistic Regression...")
 lr = LogisticRegression(multi_class='multinomial')
 lr.fit(X_train, Y_train)
 print("LR TF-IDF: " + str(lr.score(X_test, Y_test)))
-"""
--- ML Analysis/bert_twitter.py
+++ ML Analysis/bert_twitter.py
@@ -4,9 +4,19 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 
-df = pd.read_csv("../Data/collated.csv")
+# ---------- Import data and convert categorical data to int ----------
 
-X_train, X_test, Y_train, Y_test = train_test_split(df['text'], df['class'], stratify=df['class'], random_state=0)
+df = pd.read_csv("data.csv")
+X = df['text']
+countries = df['country']
+cat = pd.Categorical(df['country'])
+Y = pd.Series(cat.codes)
+
+# ---------- Split data ----------
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, random_state=0)
+
+# ---------- Setup BERT ----------
 
 bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
 bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
@@ -18,13 +28,13 @@ preprocess_text = bert_preprocess(text_input)
 outputs = bert_encoder(preprocess_text)
 print("BERT layers made.")
 
-l =tf.keras.layers.Dropout(0.3, name='dropout')(outputs['pooled_output'])
+l = tf.keras.layers.Dropout(0.3, name='dropout')(outputs['pooled_output'])
 l = tf.keras.layers.Dense(1, activation='sigmoid', name="output")(l)
 print("NN layers made.")
 
 model = tf.keras.Model(inputs=[text_input], outputs=[l])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, Y_train, epochs=7, batch_size=16)
+model.fit(X_train, Y_train, epochs=1, batch_size=16)
 print("Model made.")
 
 Y_predict = model.predict(X_test)
